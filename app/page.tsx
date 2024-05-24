b157d3df7b9406e99e5c55774fab94fac4420a4d
insu <hinsu93@gmail.com>
2024-05-24 14:58:23 +0900

    More Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@ export default function Home() {
     <main className="flex h-screen items-center justify-center bg-slate-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-gray-700">
       <div className="flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 shadow-lg md:flex-row dark:bg-gray-600">
         <input
-          type="text"
+          type="email"
           placeholder="Search here..."
-          className="h-10 w-full rounded-full bg-gray-200 pl-5 outline-none ring ring-transparent transition-shadow placeholder:drop-shadow focus:ring-orange-500 focus:ring-offset-2"
+          className="peer h-10 w-full rounded-full bg-gray-200 pl-5 outline-none ring ring-transparent transition-shadow placeholder:drop-shadow focus:ring-orange-500 focus:ring-offset-2 invalid:focus:ring-red-500"
+          required
         />
-        <button className="rounded-full bg-black py-2 font-medium text-white outline-none transition-transform focus:scale-90 active:scale-90 md:px-5">
+        <span className="hidden font-medium text-red-500 peer-invalid:block">
+          Email is required
+        </span>
+        <button className="rounded-full bg-gradient-to-tr from-cyan-500 via-yellow-300 to-purple-400 py-2 font-medium text-white outline-none transition-transform focus:scale-90 active:scale-90 peer-invalid:text-red-500 md:px-5">
           Search
         </button>
       </div>
