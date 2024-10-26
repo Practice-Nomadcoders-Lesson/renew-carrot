171daf59dcfd425d6c14acced7d70d6219ae56d7
iinsue <hinsu93@gmail.com>
2024-10-26 22:21:51 +0900

    #4.0 기본 메인페이지 HTML 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center bg-slate-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-gray-700">
-      <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg dark:bg-gray-600">
-        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-5 border-b-2 pb-5 last:border-0 last:pb-0"
-          >
-            <div className="size-10 rounded-full bg-blue-400" />
-            <span className="text-lg font-medium">{person}</span>
-            <div className="flex size-6 items-center justify-center rounded-full bg-red-500 text-white">
-              <span>{index}</span>
-            </div>
-          </div>
-        ))}
+    <div>
+      <div>
+        <span>🥕</span>
+        <h1>당근</h1>
+        <h2>당근 마켓에 어서오세요!</h2>
       </div>
-    </main>
+      <div>
+        <Link href="/create-account">시작하기</Link>
+        <div>
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login">로그인</Link>
+        </div>
+      </div>
+    </div>
   );
 }
