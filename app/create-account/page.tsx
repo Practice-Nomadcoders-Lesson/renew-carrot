0dce984da06f9b652e4d2739ff504ca654a6b6ed
iinsue <hinsu93@gmail.com>
2024-10-27 22:01:18 +0900

    #4.3 Form Components

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,3 +1,5 @@
+import { FormButton } from "@/components/form-btn";
+import { FormInput } from "@/components/form-input";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
@@ -10,18 +12,22 @@ const CreateAccountPage = () => {
       </div>
 
       <form className="flex flex-col gap-3">
-        <div className="flex flex-col gap-2">
-          <input
-            className="h-10 w-full rounded-md border-none bg-transparent ring-1 ring-neutral-200 placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-orange-500"
-            type="text"
-            placeholder="Username"
-            required
-          />
-          <span className="font-medium text-red-500">Input error</span>
-        </div>
-        <button className="primary-btn h-10">Create account</button>
+        <FormInput required type="text" errors={[]} placeholder="Username" />
+        <FormInput required type="email" errors={[]} placeholder="Email" />
+        <FormInput
+          required
+          type="password"
+          errors={[]}
+          placeholder="Password"
+        />
+        <FormInput
+          required
+          type="password"
+          errors={[]}
+          placeholder="Confirm Password"
+        />
       </form>
-
+      <FormButton loading={false} text="Create account" />
       <div className="h-px w-full bg-neutral-500" />
 
       <div>
