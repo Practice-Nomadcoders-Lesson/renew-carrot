789539e55157f21f09552bf5f6dfd28d02bac561
iinsue <hinsu93@gmail.com>
2024-10-27 21:07:31 +0900

    #4.1 회원가입 페이지 Layout

---
+++ app/create-account/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const CreateAccountPage = () => {
+  return (
+    <div>
+      <div>
+        <h1>안녕하세요!</h1>
+        <h2>Fill in the form below to join!</h2>
+      </div>
+      <form>
+        <div>
+          <input type="text" placeholder="Username" required />
+          <span>Input error</span>
+        </div>
+        <button>Create account</button>
+      </form>
+      <div />
+      <div>
+        <Link href="/sms">
+          <span>icon</span>
+          <span>Sign up with SMS</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CreateAccountPage;
