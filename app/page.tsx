7fcc16dc51c4a65763b00d291edd5148b8b2d7bd
iinsue <hinsu93@gmail.com>
2024-10-27 20:32:55 +0900

    #4.0 기본 메인페이지 스타일 추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,24 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <span>🥕</span>
-        <h1>당근</h1>
-        <h2>당근 마켓에 어서오세요!</h2>
+    <div className="flex min-h-screen flex-col items-center justify-between p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
       </div>
-      <div>
-        <Link href="/create-account">시작하기</Link>
-        <div>
+      <div className="flex w-full flex-col items-center gap-2">
+        <Link
+          href="/create-account"
+          className="w-full rounded-md bg-orange-500 py-2.5 text-center text-lg font-medium text-white transition-colors hover:bg-orange-400"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2 ">
           <span>이미 계정이 있나요?</span>
-          <Link href="/login">로그인</Link>
+          <Link href="/login" className="underline-offset-4 hover:underline">
+            로그인
+          </Link>
         </div>
       </div>
     </div>
