db0441fe7f81ac98c776f952f11c496019587fb0
iinsue <hinsu93@gmail.com>
2024-10-28 22:30:31 +0900

    #4.3 Login Screen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,16 +2,15 @@ import { FormButton } from "@/components/form-btn";
 import { FormInput } from "@/components/form-input";
 import { SocialLogin } from "@/components/social-login";
 
-const CreateAccountPage = () => {
+const LoginPage = () => {
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">Fill in the form below to join!</h2>
+        <h2 className="text-xl">Log in with email and password</h2>
       </div>
 
       <form className="flex flex-col gap-3">
-        <FormInput required type="text" errors={[]} placeholder="Username" />
         <FormInput required type="email" errors={[]} placeholder="Email" />
         <FormInput
           required
@@ -19,17 +18,11 @@ const CreateAccountPage = () => {
           errors={[]}
           placeholder="Password"
         />
-        <FormInput
-          required
-          type="password"
-          errors={[]}
-          placeholder="Confirm Password"
-        />
-        <FormButton loading={false} text="Create account" />
+        <FormButton loading={false} text="Log in" />
       </form>
       <SocialLogin />
     </div>
   );
 };
 
-export default CreateAccountPage;
+export default LoginPage;
