dd86c61ea819ca1aa2a9f9fef43ef109fb19236a
insu <hinsu93@gmail.com>
2024-11-15 08:56:23 +0900

    #6.8 SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,12 +2,49 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format",
+  );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export const smsLogin = async (prevState: any, formData: FormData) => {
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
+interface ActionState {
+  token: boolean;
+}
+
+export const smsLogin = async (prevState: ActionState, formData: FormData) => {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+
+  // prevState.token 이 false이면 초기값이 넘어온 것으로 이 action은 처음호출되었다는 의미입니다.
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      // 로그인 성공
+      redirect("/");
+    }
+  }
 };
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -7,8 +7,13 @@ import { Input } from "@/components/input";
 
 import { smsLogin } from "./actions";
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 const SMSLoginPage = () => {
-  const [state, dispatch] = useFormState(smsLogin, null);
+  const [state, dispatch] = useFormState(smsLogin, initialState);
 
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
@@ -18,16 +23,25 @@ const SMSLoginPage = () => {
       </div>
 
       <form className="flex flex-col gap-3" action={dispatch}>
-        <Input required type="text" name="phone" placeholder="Phone number" />
-        <Input
-          required
-          type="number"
-          name="token"
-          min={100000}
-          max={999999}
-          placeholder="Verification code"
-        />
-        <Button text="Verify" />
+        {state.token ? (
+          <Input
+            required
+            type="number"
+            name="token"
+            min={100000}
+            max={999999}
+            placeholder="Verification code"
+          />
+        ) : (
+          <Input
+            required
+            type="text"
+            name="phone"
+            placeholder="Phone number"
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
