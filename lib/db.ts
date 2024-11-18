f32289242e561c60a89260d26006ac1fae23cfd8
insu <hinsu93@gmail.com>
2024-11-18 09:55:20 +0900

    #7.4 Relationships

--- lib/db.ts
+++ lib/db.ts
@@ -3,14 +3,12 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function test() {
-  const token = await db.sMSToken.create({
-    data: {
-      token: "121212",
-      user: {
-        connect: {
-          id: 1,
-        },
-      },
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
     },
   });
   console.log(token);
