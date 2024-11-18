e88b99cfa5ae4d81eb40bb732b261abe4386ea4b
insu <hinsu93@gmail.com>
2024-11-18 09:03:42 +0900

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -2,16 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  const user = await db.user.create({
-    data: {
-      username: "user-test",
-    },
-  });
-
-  console.log(user);
-}
-
-test();
-
 export default db;
