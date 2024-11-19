0b2b3ab0d19141bd6eedaaf5b4fdf8bd9f5b41a8
insu <hinsu93@gmail.com>
2024-11-19 18:03:43 +0900

    #8.5 Email Log in

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,14 +1,37 @@
 "use server";
 
 import { z } from "zod";
+import bcrypt from "bcrypt";
+
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+// find a user with the email
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, "An account with this email does not exists."),
   password: z
     .string({ required_error: "Password is required" })
     .min(PASSWORD_MIN_LENGTH)
@@ -21,15 +44,44 @@ export const login = async (prevState: any, formData: FormData) => {
     password: formData.get("password"),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
 
   if (!result.success) {
     return result.error.flatten();
   } else {
-    // find a user with the email
     // if the user is found, check password hash
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+
+    //check password hash
+    const ok = await bcrypt.compare(
+      result.data.password,
+      user!.password ?? "xxxx",
+    );
+
     // log the user in
-    // redirect "/profile"
-    console.log(result.data);
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+
+      // redirect "/profile"
+      redirect("/profile");
+    } else {
+      // 비밀번호가 틀렸다면
+      // zod모양과 같은 형태로 에러를 리턴합니다.
+      return {
+        fieldErrors: {
+          password: ["Wrong password."],
+          email: [],
+        },
+      };
+    }
   }
 };
