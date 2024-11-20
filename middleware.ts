210aa1a0980fcf26a6b91ba1fb5d7df4a5cd0ec9
insu <hinsu93@gmail.com>
2024-11-20 17:55:12 +0900

    #8.12 Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+/* 
+object내에서 뭔가를 포함하고 있는지 검색하는 게,
+array내에서 뭔가를 포함하고 있나 검색하는 것보다 약간 더 빠르기 때문에
+array가 아닌 object로 구성합니다.
+*/
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
-  console.log("Hello");
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    // 사용자가 로그인상태 일 경우
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+  }
 }
 
 export const config = {
