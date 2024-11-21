1026398700081fc6409314f5912cab9e1c5af96c
insu <hinsu93@gmail.com>
2024-11-21 16:18:53 +0900

    #10.2 Skeletons

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,4 +1,10 @@
-const ProductsPage = () => {
+async function getProducts() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+}
+
+const ProductsPage = async () => {
+  const products = await getProducts();
+
   return (
     <>
       <div>Product Page</div>
