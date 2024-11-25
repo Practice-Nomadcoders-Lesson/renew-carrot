89533509655d6d3ff50bf17a72b0f76565c0e444
iinsue <hinsu93@gmail.com>
2024-11-26 02:07:11 +0900

    #11.0 Introduction

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -1,16 +1,33 @@
-import { Button } from "@/components/button";
+"use client";
+
+import { useState } from "react";
+
 import { Input } from "@/components/input";
+import { Button } from "@/components/button";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 
 const AddProductPage = () => {
+  const [preview, setPreview] = useState("");
+  const onImageChange = () => {};
+
   return (
     <div>
-      <form>
-        <label htmlFor="photo">
+      <form className="flex flex-col gap-5 p-5">
+        <label
+          htmlFor="photo"
+          className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2
+          border-dashed border-neutral-300 text-neutral-300"
+        >
           <PhotoIcon className="w-20" />
           <div className="text-sm text-neutral-400">사진을 추가해주세요.</div>
         </label>
-        <input type="file" name="photo" id="photo" />
+        <input
+          type="file"
+          name="photo"
+          id="photo"
+          className="hidden"
+          onChange={onImageChange}
+        />
         <Input type="text" name="title" required placeholder="제목" />
         <Input type="number" name="price" required placeholder="가격" />
         <Input
