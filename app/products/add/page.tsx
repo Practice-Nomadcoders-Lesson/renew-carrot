148a0fa3c4a95a8f8a817a63a37b4af63ec24c9d
iinsue <hinsu93@gmail.com>
2024-12-02 01:51:09 +0900

    #11.1 Form Action

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -5,21 +5,35 @@ import { useState } from "react";
 import { Input } from "@/components/input";
 import { Button } from "@/components/button";
 import { PhotoIcon } from "@heroicons/react/24/solid";
+import { uploadProduct } from "./actions";
 
 const AddProductPage = () => {
   const [preview, setPreview] = useState("");
-  const onImageChange = () => {};
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.currentTarget.files) return;
+
+    const file = event.currentTarget.files[0];
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
 
   return (
     <div>
-      <form className="flex flex-col gap-5 p-5">
+      <form className="flex flex-col gap-5 p-5" action={uploadProduct}>
         <label
           htmlFor="photo"
           className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2
-          border-dashed border-neutral-300 text-neutral-300"
+          border-dashed border-neutral-300 bg-cover bg-center text-neutral-300"
+          style={{ backgroundImage: `url(${preview})` }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-sm text-neutral-400">사진을 추가해주세요.</div>
+          {preview === "" && (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-sm text-neutral-400">
+                사진을 추가해주세요.
+              </div>
+            </>
+          )}
         </label>
         <input
           type="file"
