1c521d7afbed32210eaedc4f8e8d4a52aa6b4c80
iinsue <hinsu93@gmail.com>
2025-01-10 23:14:03 +0900

    #14.1 See Posts Get List

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,9 +1,32 @@
+import db from "@/lib/db";
+
 async function getPosts() {
-  await new Promise((resolve) => setTimeout(resolve, 100000));
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+    },
+  });
+
+  return posts;
 }
+
+export const metadata = {
+  title: "동네생활",
+};
 
 export default async function Life() {
   const posts = await getPosts();
+  console.log(posts);
 
   return (
     <div>
