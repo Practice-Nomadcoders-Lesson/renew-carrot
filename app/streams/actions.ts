7dda39aa54c98c3c681ef78fd39118d5fab12c75
iinsue <hinsu93@gmail.com>
2025-01-13 06:43:42 +0900

    #16.1 Live Input

--- app/streams/actions.ts
+++ app/streams/actions.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
 import { z } from "zod";
 
 const title = z.string().min(10);
@@ -9,4 +12,38 @@ export async function startStream(_: any, formData: FormData) {
   if (!results.success) {
     return results.error.flatten();
   }
+
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/stream/live_inputs`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CLOUDFLARE_TOKEN}`,
+      },
+      body: JSON.stringify({
+        meta: {
+          name: results.data,
+        },
+        recording: {
+          mode: "automatic",
+        },
+      }),
+    },
+  );
+
+  const data = await response.json();
+  const session = await getSession();
+  const stream = await db.liveStream.create({
+    data: {
+      title: results.data,
+      stream_id: data.result.uid,
+      stream_key: data.result.rtmps.streamKey,
+      userId: session.id!,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  redirect(`/streams/${stream.id}`);
 }
