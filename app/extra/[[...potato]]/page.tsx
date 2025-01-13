890631f2bd7cf4b8664706124f089cbece965ab2
iinsue <hinsu93@gmail.com>
2025-01-14 07:22:04 +0900

    #17.4 Logging

--- app/extra/[[...potato]]/page.tsx
+++ app/extra/[[...potato]]/page.tsx
@@ -1,9 +1,31 @@
-export default function Extras({ params }: { params: { potato: string[] } }) {
+import { revalidatePath } from "next/cache";
+
+async function getData() {
+  const data = await fetch(
+    "https://nomad-movies.nomadcoders.workers.dev/movies",
+  );
+}
+
+export default async function Extras({
+  params,
+}: {
+  params: { potato: string[] };
+}) {
+  await getData();
+
+  const action = async () => {
+    "use server";
+    revalidatePath("/extra");
+  };
+
   return (
     <div className="flex flex-col gap-3 py-10">
       <h1 className="font-rubik text-6xl">Extras!</h1>
       <h2 className="font-roboto">So much more to learn!</h2>
       <h3 className="font-pretendard">세상에 이런 폰트가 나오다니 천재인듯</h3>
+      <form action={action}>
+        <button className="rounded-md border px-2 uppercase">revalidate</button>
+      </form>
     </div>
   );
 }
